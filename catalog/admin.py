d0291ba85770e6e5089b8d75fcf9f6a3ac4e5af0
developerandrii <workerandrii@gmail.com>
2026-07-05 11:19:40 +0300

    Register novel model in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from catalog.models import Novel
 
-# Register your models here.
+
+class NovleAdmin(admin.ModelAdmin):
+    list_display = ["title", "format", "status"]
+    list_filter = ["status", "format"]
+    search_fields = ["title",]
+    
+
+admin.site.register(Novel, NovleAdmin)
