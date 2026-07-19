c6cebd64b2d5178b51710ea35e78b8fc68db82d9
developerandrii <workerandrii@gmail.com>
2026-07-19 20:45:44 +0300

    Add chapter create form page

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
+from django.shortcuts import get_object_or_404
 from .models import Novel, Chapter
 
 
@@ -41,3 +42,28 @@ class ChapterDetailView(DetailView):
         )
 
         return context
+    
+
+class ChapterCreateView(CreateView):
+    model = Chapter
+    template_name = 'catalog/chapter_form.html'
+    fields = [
+        'title',
+        'number',
+        'content',
+    ]
+
+    def form_valid(self, form):
+        novel = get_object_or_404(Novel, public_id=self.kwargs.get('public_id'))
+        form.instance.novel = novel
+        submitted_number = form.cleaned_data.get('number')
+
+        duplicate_exists = Chapter.objects.filter(
+            novel=novel, 
+            number=submitted_number
+        ).exists()
+
+        if duplicate_exists:
+            form.add_error('number', f"Chapter {submitted_number} already exists for this novel.")
+            return self.form_invalid(form)
+        return super().form_valid(form)
