c9c3d4429d5e09c6045ed582bb1f49fb0a13f8b1
developerandrii <workerandrii@gmail.com>
2026-07-23 19:31:32 +0300

    add django-extansions libary

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+
     'core.apps.CoreConfig',
     'users.apps.UsersConfig',
     'catalog.apps.CatalogConfig',
