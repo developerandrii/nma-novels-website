44341a418886d92183dce7ea54050d1be341a7c8
developerandrii <workerandrii@gmail.com>
2026-08-16 16:34:42 +0300

    Update chapter list styles for modal in chapter detail

--- catalog/static/catalog/js/chapter-list.js
+++
@@ -1,5 +0,0 @@
-const chapterListButton = document.querySelector("[data-chapter-list-modal-button]");
-const settingsModal = document.querySelector("[data-chapter-list-modal]");
-const closeButton = document.querySelector("[data-chapter-list-modal-close]");
-
-closeButton.addEventListener("input", () => chapterListButton.hidden = true);
--- catalog/static/catalog/js/chapter_detail/chapter-list-modal.js
+++ catalog/static/catalog/js/chapter_detail/chapter-list-modal.js
@@ -1,6 +1,13 @@
 const chapterListButton = document.querySelector("[data-chapter-list-button]");
 const chapterListModal = document.querySelector("[data-chapter-list-modal]");
 const closeChapterListButton = document.querySelector("[data-chapter-list-modal-close]");
+const currentChapter = document.querySelector('[data-current-chapter]')
 
-chapterListButton.addEventListener("click", () => chapterListModal.hidden = false)
+chapterListButton.addEventListener("click", () => {
+    chapterListModal.hidden = false;
+    currentChapter.scrollIntoView({
+        block: 'center',
+        behavior: 'instant'
+    })
+})
 closeChapterListButton.addEventListener("click", () => chapterListModal.hidden = true);
